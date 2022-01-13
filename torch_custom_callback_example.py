290212d3bca585bfea25d7de5c07e54b5860c633
MusaTamzid05 <tamzid101@gmail.com>
2022-01-13 17:23:21 +0600

    mnist dataset class for pytorch.

---
+++ torch_custom_callback_example.py
@@ -0,0 +1,42 @@
+from torch.utils.data import Dataset
+import os
+import gzip
+import numpy as np
+
+def read_gzip(data_path):
+    with open(data_path, "rb") as f:
+        content = f.read()
+
+
+    return content
+
+class MnistDataset(Dataset):
+    def __init__(self, dir_path , validation_type = False):
+        if validation_type:
+            self._init_validation(dir_path = dir_path)
+        else:
+            self._init_training(dir_path = dir_path)
+
+        print(self.x.shape)
+        print(self.y.shape)
+
+
+
+    def _init_validation(self , dir_path):
+        with gzip.open(os.path.join(dir_path, "t10k-labels-idx1-ubyte.gz"), "rb") as f:
+            self.y = np.frombuffer(f.read(), np.uint8, offset = 8)
+
+        with gzip.open(os.path.join(dir_path, "t10k-images-idx3-ubyte.gz"), "rb") as f:
+            self.x = np.frombuffer(f.read(), np.uint8, offset = 16).reshape(len(self.y), 28, 28)
+
+
+    def _init_training(self , dir_path):
+        with gzip.open(os.path.join(dir_path, "train-labels-idx1-ubyte.gz"), "rb") as f:
+            self.y = np.frombuffer(f.read(), np.uint8, offset = 8)
+
+        with gzip.open(os.path.join(dir_path, "train-images-idx3-ubyte.gz"), "rb") as f:
+            self.x = np.frombuffer(f.read(), np.uint8, offset = 16).reshape(len(self.y), 28, 28)
+
+if __name__ == "__main__":
+    dataset = MnistDataset(dir_path = "./datasets/mnist", validation_type = True)
+    dataset = MnistDataset(dir_path = "./datasets/mnist")
